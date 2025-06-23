5de2e4ae07852b35a310ea1cf25a1491968b1d9a
230227 <230227@jc-21.jp>
2025-06-23 13:03:05 +0900

    ステージセレクトのcsv読み込み調整中

--- StageSelectScene.cpp
+++ StageSelectScene.cpp
@@ -6,8 +6,12 @@
 
 namespace {
 	int MAX_STAGE_NUM = 3; // 最大ステージ数
-	int STAGE_IMAGE_WIDTH = 200; // ステージ画像の幅
-	int STAGE_IMAGE_SPACING = 20; // ステージ画像の間隔
+	int STAGE_IMAGE_WIDTH = 3; // ステージ画像の幅
+	int STAGE_IMAGE_HEIGHT = 3; // ステージ画像の高さ
+	int STAGE_IMAGE_SPACING = 2; // ステージ画像の間隔
+	int STAGE_IMAGE_X_OFFSET = 5; // ステージ画像のX方向オフセット位置
+	int STAGE_IMAGE_Y_OFFSET = 5; // ステージ画像のY方向オフセット位置
+	int THEME_IMAGE_Y_OFFSET = 3; // お題画像のY方向オフセット位置
 }
 
 StageSelectScene::StageSelectScene(GameObject* parent)
@@ -20,6 +24,7 @@ StageSelectScene::StageSelectScene(GameObject* parent)
 
 void StageSelectScene::Initialize()
 {
+	//画像をロード
 	hStageSelectPict_ = Image::Load("UI/StageSelect/StageSelect.png");
 	assert(hStageSelectPict_ >= 0);
 	hStageFrameGrayPict_ = Image::Load("UI/StageSelect/StageFrame_Gray.png");
@@ -36,16 +41,22 @@ void StageSelectScene::Initialize()
 	int lines = csvStage->GetLines();
 	for (int i = 1;i < lines;i++) {
 		stageInfos_.push_back(StageInfo());
-		stageInfos_[i].fileName = csvStage->GetString(i, 0);
-		stageInfos_[i].stageNum = csvStage->GetInt(i, 1);
-		stageInfos_[i].hThemePict = Image::Load(csvStage->GetString(i, 2));
+		//ステージファイル名を取得
+		stageInfos_[i].fileName = "CSV/StageData/" + csvStage->GetString(i, 0) + ".csv";
+		//ステージのテーマ画像とステージ画像をロード
+		stageInfos_[i].hThemePict = Image::Load("UI/ThemeImage/" + csvStage->GetString(i, 1) + ".png");
 		assert(stageInfos_[i].hThemePict >= 0);
-		stageInfos_[i].hStagePict = Image::Load(csvStage->GetString(i, 3));
+		stageInfos_[i].hStagePict = Image::Load("UI/StageImage/" + csvStage->GetString(i, 2) + ".png");
 		assert(stageInfos_[i].hStagePict >= 0);
-		stageInfos_[i].x = csvStage->GetInt(i, 4);
-		stageInfos_[i].y = csvStage->GetInt(i, 5);
+		//ステージ画像の表示位置を計算
+		stageInfos_[i].stageImgTrs.position_.x = -STAGE_IMAGE_X_OFFSET + STAGE_IMAGE_WIDTH * i + STAGE_IMAGE_SPACING * i;
+		stageInfos_[i].stageImgTrs.position_.y = 0;
+		//お題画像の表示位置を計算
+		stageInfos_[i].themeImgTrs.position_.x = -STAGE_IMAGE_X_OFFSET + STAGE_IMAGE_WIDTH * i + STAGE_IMAGE_SPACING * i;
+		stageInfos_[i].themeImgTrs.position_.y = THEME_IMAGE_Y_OFFSET;
 		stageInfos_[i].isSelected = false; // 初期は選択されていない
 	}
+	stageInfos_[0].isSelected = true; // 最初のステージを選択状態にする
 }
 
 void StageSelectScene::Update()
@@ -60,16 +71,30 @@ void StageSelectScene::Update()
 
 void StageSelectScene::Draw()
 {
-	Image::SetTransform(hStageSelectPict_, transform_);
+	Image::SetTransform(hStageSelectPict_, stageSelectPictTrs_);
 	Image::Draw(hStageSelectPict_);
-	Image::SetTransform(hStageFrameGrayPict_, transform_);
-	Image::Draw(hStageFrameGrayPict_);
-	Image::SetTransform(hStageFrameYellowPict_, transform_);
-	Image::Draw(hStageFrameYellowPict_);
-	Image::SetTransform(hExplUIGrayPict_, transform_);
+	Image::SetTransform(hExplUIGrayPict_, explUITrs_);
 	Image::Draw(hExplUIGrayPict_);
-	Image::SetTransform(hExplUIYellowPict_, transform_);
+	Image::SetTransform(hExplUIYellowPict_, explUITrs_);
 	Image::Draw(hExplUIYellowPict_);
+	// ステージ情報を描画
+	for (int i = 0; i < stageInfos_.size(); i++) {
+		// ステージのテーマ画像を描画
+		Image::SetTransform(stageInfos_[i].hThemePict, stageInfos_[i].themeImgTrs);
+		Image::Draw(stageInfos_[i].hThemePict);
+		// 選択されているステージのフレームを描画
+		if (stageInfos_[i].isSelected) {
+			Image::SetTransform(hStageFrameYellowPict_, stageFrameTrs_);
+			Image::Draw(hStageFrameYellowPict_);
+		}
+		else {
+			Image::SetTransform(hStageFrameGrayPict_, stageFrameTrs_);
+			Image::Draw(hStageFrameGrayPict_);
+		}
+		// ステージの画像を描画
+		Image::SetTransform(stageInfos_[i].hStagePict, stageInfos_[i].stageImgTrs);
+		Image::Draw(stageInfos_[i].hStagePict);
+	}
 }
 
 void StageSelectScene::Release()
--- StageSelectScene.h
+++ StageSelectScene.h
@@ -1,9 +1,11 @@
 #pragma once
 #include "Engine/GameObject.h"
+#include <vector>
+
 class StageSelectScene :
 	public GameObject
 {
-	int hStageSelectPict_; //StageSelect画像のハンドル
+	int hStageSelectPict_; //ステージセレクト画像のハンドル
 	int hStageFrameGrayPict_; //ステージフレーム画像(灰色)のハンドル
 	int hStageFrameYellowPict_; //ステージフレーム画像(黄色)のハンドル
 	int hExplUIGrayPict_; //操作・ルール説明画像(灰色)のハンドル
@@ -12,15 +14,19 @@ class StageSelectScene :
 	//ステージ情報をまとめた構造体
 	struct StageInfo {
 		std::string fileName; // ステージのCSVファイル名
-		int stageNum; // ステージ番号
 		int hThemePict; // ステージのテーマ画像ハンドル
 		int hStagePict; // ステージの画像ハンドル
-		int x, y; // 画像の表示位置
+		Transform stageImgTrs; // ステージ画像の表示位置
+		Transform themeImgTrs; // お題画像の表示位置
 		bool isSelected; // ステージが選択されているかどうか
 	};
 	
 	int currentStageIndex_; // 現在選択中のステージインデックス
 	std::vector<StageInfo> stageInfos_; // ステージ情報のリスト
+
+	Transform stageSelectPictTrs_; // ステージ選択画面の画像のトランスフォーム
+	Transform stageFrameTrs_; // ステージフレームのトランスフォーム
+	Transform explUITrs_; // 操作・ルール説明UIのトランスフォーム
 public:
 	StageSelectScene(GameObject* parent);
 	//初期化
